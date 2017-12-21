a18e8df0b03df6bc3f7d17a6d3445a3ec8fd3430
Matej Svajger <hello@matejsvajger.com>
2017-12-21 13:38:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# konami-cb
+
+A lightweight script that will execute a callback function if a user types in the Konami Cheat code into the browser: 
+
+`up, up, down, down, left, right, left, right, b, a`
+
+**Install:**
+
+`npm i konami-cb`
+
+**Usage:**
+
+```js
+var konami = require('konami-cb');
+
+konami(function() {
+
+    //- make stuff happen
+
+});
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+var konami = function (cb) {
+
+  if (typeof cb !== 'function') {
+    return
+  }
+
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  const length = code.length
+
+  var pos = 0
+
+  const check = () => (length == pos) ? fire() : reset()
+
+  const fire = () => {
+    cb()
+    reset()
+  }
+
+  const reset = () => {
+    pos = 0
+  }
+
+  document.addEventListener('keydown', ({ keyCode }) => (keyCode === kode[pos++]) ? check() : null, false)
+
+}
+
+typeof module !== 'undefined' && typeof module.exports !== 'undefined' && (module.exports = konami)
